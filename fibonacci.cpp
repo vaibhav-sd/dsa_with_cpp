eff6039e2d4fcf173319af05421a8562356a5aed
Vaibhav Dagwal <vaibhavdagwal18@gmail.com>
2022-09-15 22:11:26 +0530

    Fibonacci series code

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a = 0, b = 1;
+    cout << a << " " << b;
+
+    for (int i = 2; i < n; i++) {
+        int c = a + b;
+        cout << " " << c;
+        a = b;
+        b = c;
+    }
+    return 0;
+}
