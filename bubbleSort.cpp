bbf0ab9439a43406372705ecc91c41b56e17bc35
Vaibhav Dagwal <vaibhav.dagwal2015@gmail.com>
2023-11-07 11:31:11 +0530

    Optimized best case time complexity

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
-    for(int i = 0; i < n; i++) {
+    bool swapped = true;
+    for(int i = 0; i < n and swapped; i++) {
+        swapped = false;
         for (int j = 0; j < n-1-i; j++) {
             if(arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
     }
